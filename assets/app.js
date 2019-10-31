bcde8ae5e28ad1fc08390ce4f7100f9fdf9682ba
William Sams III <w.sams11@yahoo.com>
2019-10-31 13:51:25 -0700

    final touches and readme

--- assets/app.js
+++ assets/app.js
@@ -1,14 +1,8 @@
-// set variables
-// var $value8 = document.querySelector("#8");
 
-// first get the save button to save info to localstorage 
 $(".button").on('click', function () {
     var valueText = $(this).siblings(".textContent").val();
     var time = $(this).parent().attr("id");
-    // console.log(valueText);
-    // console.log(time);
     localStorage.setItem(time, valueText);
-    // console.log(localStorage.getItem("hour-8"));
 })
 $("#hour-8 .textContent").val(localStorage.getItem("hour-8"));
 $("#hour-9 .textContent").val(localStorage.getItem("hour-9"));
@@ -20,12 +14,3 @@ $("#hour-2.textContent").val(localStorage.getItem("hour-2"));
 $("#hour-3.textContent").val(localStorage.getItem("hour-3"));
 $("#hour-4.textContent").val(localStorage.getItem("hour-4"));
 $("#hour-5.textContent").val(localStorage.getItem("hour-5"));
-// when the site loads, check for info in local storage
-// if it finds info, load in for time text box
-// console.log(moment());
-
-
-
-// moment.js for the color on time 
-// get moment cdn
-// run 
